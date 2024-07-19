cd7d52551343c6669acb3ce5cd7b3f9f235b3ba5
yubraj-rai <raiyubrj@gmail.com>
2024-07-18 21:34:38 -0400

    Added: newcomponent to the project

---
+++ simple-react-app/src/ExampleComponent.js
@@ -0,0 +1,13 @@
+// src/ExampleComponent.js
+import React from 'react';
+
+const ExampleComponent = () => {
+    return (
+        <div>
+            <h2>Welcome to Example Component</h2>
+            <p>This is a simple React component.</p>
+        </div>
+    );
+};
+
+export default ExampleComponent;
